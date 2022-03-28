f64d3eeaa4f865b2c98d5dcc1052200e2a14efe0
Marius Klemp Petersen <mariuskp@stud.ntnu.no>
2022-03-28 10:05:27 +0200

    Initial commit

---
+++ src/main/java/com/example/IDATT2015QS3REST/Idatt2015Qs3RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.IDATT2015QS3REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idatt2015Qs3RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idatt2015Qs3RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IDATT2015QS3REST/Idatt2015Qs3RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IDATT2015QS3REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idatt2015Qs3RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
