487c5423e2eb9ab4453edde836f06011579b6106
Simen Klemp <simenk2312@gmail.com>
2022-03-29 11:36:37 +0200

    Implemented getAllSubjects method

--- src/main/java/com/example/IDATT2015QS3REST/repository/SubjectRepository.java
+++ src/main/java/com/example/IDATT2015QS3REST/repository/SubjectRepository.java
@@ -2,6 +2,10 @@ package com.example.IDATT2015QS3REST.repository;
 
 import com.example.IDATT2015QS3REST.model.Subject;
 
+import java.util.List;
+
 public interface SubjectRepository {
     int addSubject(Subject subject);
+
+    List<Subject> getAllSubjects(int id);
 }
