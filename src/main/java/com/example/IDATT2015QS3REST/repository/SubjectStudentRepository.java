fe3db8093a0c1ee156300c6305768a0d9ce3a1dd
Simen Klemp <simenk2312@gmail.com>
2022-03-31 12:33:11 +0200

    Implemented method for retrieving all students belonging to a subject

--- src/main/java/com/example/IDATT2015QS3REST/repository/SubjectStudentRepository.java
+++ src/main/java/com/example/IDATT2015QS3REST/repository/SubjectStudentRepository.java
@@ -1,10 +1,15 @@
 package com.example.IDATT2015QS3REST.repository;
 
 import com.example.IDATT2015QS3REST.model.SubjectUser;
+import com.example.IDATT2015QS3REST.model.User;
+
+import java.util.List;
 
 
 public interface SubjectStudentRepository {
 
     int addStudents(SubjectUser subjectUser);
+
+    List<User> getUsersSubject(int subjectId);
 
 }
--- src/main/java/com/example/IDATT2015QS3REST/service/SubjectStudentService.java
+++ src/main/java/com/example/IDATT2015QS3REST/service/SubjectStudentService.java
@@ -7,6 +7,9 @@ import com.example.IDATT2015QS3REST.repository.SubjectStudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class SubjectStudentService {
 
@@ -15,6 +18,15 @@ public class SubjectStudentService {
 
     public int addStudents(SubjectUser subjectUser) {
         return subjectStudentRepository.addStudents(subjectUser);
+    }
+
+    public List<User> getUsersSubject(int subjectId){
+        List<User> users = new ArrayList<>();
+
+        subjectStudentRepository.getUsersSubject(subjectId).forEach(users::add);
+        System.out.println(users.size());
+        return users;
+
     }
 }
 
