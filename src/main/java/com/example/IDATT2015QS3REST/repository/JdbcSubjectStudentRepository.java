881ca395680565a87c502df6504a165205b16a79
Marius Klemp Petersen <mariuskp@stud.ntnu.no>
2022-04-04 15:05:12 +0200

    Updated javadoc

--- src/main/java/com/example/IDATT2015QS3REST/repository/JdbcSubjectStudentRepository.java
+++ src/main/java/com/example/IDATT2015QS3REST/repository/JdbcSubjectStudentRepository.java
@@ -11,27 +11,38 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+/**
+ * A class that represents a repository for communication with subjectUser database
+ */
 @Repository
 public class JdbcSubjectStudentRepository implements SubjectStudentRepository{
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    /**
+     * A method for adding a student to a subject.
+     * @param lastName The lastname of the student who is added to the subject
+     * @param name The firstname of the student who is added to the subject
+     * @param subjectId The subjectId
+     * @return status int
+     */
     @Override
     public int addStudent(String lastName, String name, int subjectId) {
 
+        //Finding the userId to the student
         User user1 = jdbcTemplate.queryForObject("SELECT userID FROM users WHERE name=? AND lastName=?",
                 BeanPropertyRowMapper.newInstance(User.class), name, lastName);
 
         int userID = user1.getUserID();
 
+        //Finding the total assignment amount for a subject
         Subject subject = jdbcTemplate.queryForObject("SELECT assignmentAmount FROM subject WHERE subjectId=?",
                 BeanPropertyRowMapper.newInstance(Subject.class),subjectId);
 
         int assignmentAmount = subject.getAssignmentAmount();
 
-
+        //Connecting all the assignments in a subject to the student
         for(int i=0; i < assignmentAmount; i++){
             Assignment assignment = jdbcTemplate.queryForObject("SELECT assignmentId FROM assignment WHERE subjectId=? AND assignmentNumber=?",
                     BeanPropertyRowMapper.newInstance(Assignment.class),subjectId, i+1);
@@ -42,17 +53,31 @@ public class JdbcSubjectStudentRepository implements SubjectStudentRepository{
                     new Object[] {userID, assignmentId, false});
         }
 
+        //Adding the student to the subject
         return jdbcTemplate.update("INSERT INTO subjectUser (userId, subjectId) VALUES(?,?)",
                 new Object[] {userID, subjectId});
 
     }
 
+    /**
+     * A method for creating a student
+     * @param lastName The lastname of the student who is created
+     * @param name The firstname of the student who is created
+     * @param email The email of the student who is created
+     * @return status int
+     */
     @Override
     public int createStudent(String lastName, String name, String email) {
         return jdbcTemplate.update("INSERT INTO users (name, lastName, email, role) VALUES(?,?,?, DEFAULT)",
                 new Object[]{name, lastName, email});
     }
 
+    /**
+     * A method for checking if the user already exist in the database
+     * @param lastName The lastname of the student
+     * @param name The firstname the student
+     * @return status int
+     */
     @Override
     public boolean findUser(String lastName, String name) {
         try {
@@ -65,13 +90,18 @@ public class JdbcSubjectStudentRepository implements SubjectStudentRepository{
         }
     }
 
+    /**
+     * A method for finding a user in a subject
+     * @param lastName The lastname of the student
+     * @param name The fistname of the student
+     * @param subjectid The subjectId of the subject
+     * @return
+     */
     @Override
     public boolean findUserInSubject(String lastName, String name, int subjectid) {
         try {
             int userId = jdbcTemplate.queryForObject("SELECT userID FROM users WHERE name=? AND lastName=?",
                     new Object[] {name, lastName}, Integer.class);
-
-            System.out.println("findUserInSubject med id " + userId);
 
             User user = jdbcTemplate.queryForObject("SELECT * FROM subjectUser WHERE userId=? AND subjectId=?",
                     BeanPropertyRowMapper.newInstance(User.class), userId, subjectid);
@@ -82,6 +112,11 @@ public class JdbcSubjectStudentRepository implements SubjectStudentRepository{
         }
     }
 
+    /**
+     * Method for adding teacher or student assistance to a subject
+     * @param subjectUser The subjectUser object that hold userDetails and subjectId
+     * @return status int
+     */
     @Override
     public int addTeacher(SubjectUser subjectUser) {
 
@@ -99,6 +134,11 @@ public class JdbcSubjectStudentRepository implements SubjectStudentRepository{
 
     }
 
+    /**
+     * A method for fetching all users in a subject
+     * @param subjectId The subjectId for the subject
+     * @return a list of users
+     */
     @Override
     public List<User> getUsersSubject(int subjectId){
         String sql = ("SELECT users.userId, users.name, users.email, users.role FROM users JOIN subjectUser ON(subjectUser.userId = users.userId) WHERE subjectUser.subjectId=? AND (users.role ='Student' OR users.role = 'Studass')");
--- src/main/java/com/example/IDATT2015QS3REST/repository/SubjectStudentRepository.java
+++ src/main/java/com/example/IDATT2015QS3REST/repository/SubjectStudentRepository.java
@@ -5,19 +5,58 @@ import com.example.IDATT2015QS3REST.model.User;
 
 import java.util.List;
 
-
+/**
+ * An interface that represents SubjectStudentRepository
+ */
 public interface SubjectStudentRepository {
 
+    /**
+     * An overrideable method for adding a student to a subject
+     * @param lastName The lastname of the student who is added to the subject
+     * @param name The firstname of the student who is added to the subject
+     * @param subjectId The subjectId
+     * @return status int
+     */
     int addStudent(String lastName, String name, int subjectId);
 
+    /**
+     * An overrideable method for creating a student
+     * @param lastName The lastname of the student who is created
+     * @param name The firstname of the student who is created
+     * @param email The email of the student who is created
+     * @return status int
+     */
     int createStudent(String lastName, String name, String email);
 
+    /**
+     * An overrideable method for checking if a user exist in the system
+     * @param lastName The lastname of the student
+     * @param name The firstname the student
+     * @return boolean
+     */
     boolean findUser(String lastName, String name);
 
+    /**
+     * An overrideable method for checking if the user exist in a subject
+     * @param lastName The lastname of the student
+     * @param name The fistname of the student
+     * @param subjectid The subjectId of the subject
+     * @return boolean
+     */
     boolean findUserInSubject(String lastName, String name, int subjectid);
 
+    /**
+     * An overrideable method for adding a teacher to a subject
+     * @param subjectUser The subjectUser object that hold userDetails and subjectId
+     * @return status int
+     */
     int addTeacher(SubjectUser subjectUser);
 
+    /**
+     * An overrideable method for fetching all students and stud.ass in a subject
+     * @param subjectId The subjectId for the subject
+     * @return A list of users
+     */
     List<User> getUsersSubject(int subjectId);
 
 }
